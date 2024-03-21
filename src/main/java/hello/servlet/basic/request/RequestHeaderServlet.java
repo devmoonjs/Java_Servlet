66baa3ac51ff5e3e0b7920d6a7bdd9724b853e6a
MOON <moonjs1018@naver.com>
2024-03-21 16:52:48 +0900

    Request Parameter

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -14,6 +15,7 @@ public class RequestHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         printStartLine(request);
+        printHeaders(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -28,6 +30,25 @@ public class RequestHeaderServlet extends HttpServlet {
         //username=hi
         System.out.println("request.getQueryString() = " + request.getQueryString());
         System.out.println("request.isSecure() = " + request.isSecure()); //https 사용유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    // Header 모든 정보
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("---Headers - start ---");
+
+/*
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + " : " + headerName);
+        }
+*/
+        request.getHeaderNames().asIterator()
+                        .forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
+
+        System.out.println("---Headers - end ---");
         System.out.println();
     }
 }
